124d3515d26f2460d5a4e261b0b557522e8df4dd
Deo Narayan <deonara447@ed.amdsb.ca>
2020-06-01 21:17:41 -0400

    instuctions screen complete

--- Deop.io/InstructionScreen.Designer.cs
+++ Deop.io/InstructionScreen.Designer.cs
@@ -28,9 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InstructionScreen));
             this.titleLabel = new System.Windows.Forms.Label();
-            this.textLabel = new System.Windows.Forms.Label();
+            this.player1TextLabel = new System.Windows.Forms.Label();
             this.mainScreenLabel = new System.Windows.Forms.Label();
+            this.player2TextLabel = new System.Windows.Forms.Label();
+            this.player2TitleLabel = new System.Windows.Forms.Label();
+            this.player1TitleLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // titleLabel
@@ -44,33 +48,65 @@
             this.titleLabel.Text = "Instructions";
             this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // textLabel
+            // player1TextLabel
             // 
-            this.textLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.5F);
-            this.textLabel.Location = new System.Drawing.Point(0, 167);
-            this.textLabel.Name = "textLabel";
-            this.textLabel.Size = new System.Drawing.Size(1200, 354);
-            this.textLabel.TabIndex = 2;
-            this.textLabel.Text = "Insert instructions here:";
+            this.player1TextLabel.Font = new System.Drawing.Font("Verdana", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player1TextLabel.Location = new System.Drawing.Point(3, 188);
+            this.player1TextLabel.Name = "player1TextLabel";
+            this.player1TextLabel.Size = new System.Drawing.Size(1203, 206);
+            this.player1TextLabel.TabIndex = 2;
+            this.player1TextLabel.Text = resources.GetString("player1TextLabel.Text");
             // 
             // mainScreenLabel
             // 
             this.mainScreenLabel.Font = new System.Drawing.Font("Segoe UI", 18F);
             this.mainScreenLabel.ForeColor = System.Drawing.Color.Green;
-            this.mainScreenLabel.Location = new System.Drawing.Point(0, 554);
+            this.mainScreenLabel.Location = new System.Drawing.Point(-3, 633);
             this.mainScreenLabel.Name = "mainScreenLabel";
             this.mainScreenLabel.Size = new System.Drawing.Size(1200, 68);
             this.mainScreenLabel.TabIndex = 13;
             this.mainScreenLabel.Text = "return to main menu";
             this.mainScreenLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // player2TextLabel
+            // 
+            this.player2TextLabel.Font = new System.Drawing.Font("Verdana", 10.125F);
+            this.player2TextLabel.Location = new System.Drawing.Point(3, 434);
+            this.player2TextLabel.Name = "player2TextLabel";
+            this.player2TextLabel.Size = new System.Drawing.Size(1197, 199);
+            this.player2TextLabel.TabIndex = 14;
+            this.player2TextLabel.Text = resources.GetString("player2TextLabel.Text");
+            // 
+            // player2TitleLabel
+            // 
+            this.player2TitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.5F);
+            this.player2TitleLabel.Location = new System.Drawing.Point(6, 394);
+            this.player2TitleLabel.Name = "player2TitleLabel";
+            this.player2TitleLabel.Size = new System.Drawing.Size(1200, 40);
+            this.player2TitleLabel.TabIndex = 15;
+            this.player2TitleLabel.Text = "2 Player";
+            this.player2TitleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // player1TitleLabel
+            // 
+            this.player1TitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.5F);
+            this.player1TitleLabel.Location = new System.Drawing.Point(3, 148);
+            this.player1TitleLabel.Name = "player1TitleLabel";
+            this.player1TitleLabel.Size = new System.Drawing.Size(1200, 40);
+            this.player1TitleLabel.TabIndex = 16;
+            this.player1TitleLabel.Text = "1 Player";
+            this.player1TitleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // InstructionScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.player1TitleLabel);
+            this.Controls.Add(this.player2TitleLabel);
+            this.Controls.Add(this.player1TextLabel);
+            this.Controls.Add(this.player2TextLabel);
             this.Controls.Add(this.mainScreenLabel);
-            this.Controls.Add(this.textLabel);
             this.Controls.Add(this.titleLabel);
             this.Name = "InstructionScreen";
             this.Size = new System.Drawing.Size(1200, 700);
@@ -82,7 +118,10 @@
         #endregion
 
         private System.Windows.Forms.Label titleLabel;
-        private System.Windows.Forms.Label textLabel;
+        private System.Windows.Forms.Label player1TextLabel;
         private System.Windows.Forms.Label mainScreenLabel;
+        private System.Windows.Forms.Label player2TextLabel;
+        private System.Windows.Forms.Label player2TitleLabel;
+        private System.Windows.Forms.Label player1TitleLabel;
     }
 }
