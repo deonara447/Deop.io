dabfaf723dd35d9f52a8d8fad08572069448b57a
Deo Narayan <deonara447@ed.amdsb.ca>
2020-06-11 15:52:36 -0400

    moved header comment to Form1

--- Deop.io/Form1.cs
+++ Deop.io/Form1.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿/*
+ * Description: Deop.io, based on the game Diep.io is both a PvP and PvE game in which players aim to shoot their opponents. 
+ * In Deop.io squares and pentagons spawn randomly and when destroyed, players receive XP. After enough XP is obtained,
+ * players level up allowing them to choose to improve an attribute. Destroy the mega square and a tremendous amount of
+ * XP is obtained! When a player dies, they respawn at half their original XP. The game ends once a player reaches 8000 XP.
+ * In the one player game, the objective is to reach as much XP as possible before you die. When bots die, they respawn 
+ * with an upgraded attribute, thus the game gets gradually more difficult. Reach a high score and you'll be on the 
+ * leaderboards.
+ * Author: Deo Narayan         
+ * Date: June 8, 2020    
+ */
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
